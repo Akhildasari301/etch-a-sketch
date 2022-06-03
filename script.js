9fa60c5814231b94736d7743a0ce89bafe011cc6
Akhil <dasariakhil2001@gmail.com>
2022-06-03 13:34:48 +0530

    Add Functionality to the buttons

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ gridSize = 16;
 let container_height = container_width = 400;
 container.style.width = container.style.height = `${container_width}px`;
 let clearbtn = document.querySelector('.clearbtn');
-
+let default_color = "lightblue";
+let cellColor = default_color;
+let color_mode = 'Fixed';
+let randColorBtn = document.querySelector('.random-color');
+let currentColor = document.querySelector('.current-color');
 
 function createGrid(size) {
     for (let i = 0; i < size; i++){
@@ -26,8 +30,9 @@ function createGrid(size) {
         item.style.height = `${container_height/gridSize}px`;
     })
     for (let i of cells){
-        i.addEventListener('mouseover', () => changeColor(i, getRandomColor()));
+        i.addEventListener('mouseover', () => changeColor(i));
     }
+    currentColor.textContent = `Current color mode: ${color_mode}`;
     
 }
 
@@ -38,8 +43,12 @@ function getRandomColor() {
     return `rgb(${cR}, ${cG}, ${cB})`;
 }
 
-function changeColor(i, color) {
-    i.style.backgroundColor = color;
+function changeColor(i) {
+    if (color_mode == 'Fixed'){
+        i.style.backgroundColor = cellColor;
+    } else {
+        i.style.backgroundColor = getRandomColor();
+    }
 }
 
 function clearGrid() {
@@ -59,3 +68,17 @@ gridSizeChange.addEventListener('click', () => {
 window.onload = () => {
     createGrid(gridSize);
 }
+
+
+let colorChangeBtn = document.getElementById('randColor');
+colorChangeBtn.oninput = () => {
+    color_mode = "Fixed";
+    cellColor = colorChangeBtn.value;
+    clearGrid();    
+    // createGrid(gridSize);
+}
+
+randColorBtn.addEventListener('click', () => {
+    color_mode = "Random";
+    clearGrid();
+})
